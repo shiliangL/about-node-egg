0819d69d795e40457087ba385d15130ec2190498
shiliangL <shiliangwei@wintelia.com>
2019-07-08 15:01:32 +0800

    api版本控制

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,15 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = 'hi, egg - 版本0';
+  }
+  async index1() {
+    const { ctx } = this;
+    ctx.body = 'hi, egg - 版本v1';
+  }
+  async index2() {
+    const { ctx } = this;
+    ctx.body = 'hi, egg - 版本v2';
   }
   async login() {
     const { ctx } = this;
--- app/router.js
+++ app/router.js
@@ -6,11 +6,12 @@
 module.exports = app => {
 
   const { router, controller } = app;
-  const routerPlus = app.router.namespace('/v1/api');
+  const routerV1 = app.router.namespace('/v1/api');
+  const routerV2 = app.router.namespace('/v2/api');
 
   // 版本控制
-  routerPlus.get('/', controller.home.index);
-  routerPlus.post('/login', controller.home.login);
+  routerV1.get('/', controller.home.index1);
+  routerV2.get('/', controller.home.index2);
 
   router.get('/', controller.home.index);
 };
