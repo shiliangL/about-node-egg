8b4dee002060392d3ba92603c7b26ce6d15bdf16
shiliangL <shiliangwei@wintelia.com>
2019-07-08 17:02:03 +0800

    前端接口访问Node后台连通

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Button } from 'antd';
+import { login} from '@/api/app'
+
+function fetchNext(params) {
+  console.log('xxxx');
+  login().then(res=>{
+    console.log(res);
+  })
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <Button type="primary">Button</Button>
+          <Button onClick={ fetchNext } type="primary">Button</Button>
         </p>
         <a
           className="App-link"
