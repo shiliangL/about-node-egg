26318d789cede956ddfb17bda33787acbbe76a9b
shiliangL <shiliangwei@wintelia.com>
2019-07-10 14:02:25 +0800

    测试登录注册页面

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React from 'react';
-import { Button } from 'antd';
-import { login, register } from '@/api/app'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginApp from '@/views/login';
+import AppLayout from '@/views/Layout';
 
-function fetchNext(params) {
-  console.log('xxxx');
-  login().then(res => {
-    console.log(res);
-  })
-}
 
-function fetchRegister(params) {
-  console.log('xxxx');
-  register().then(res => {
-    console.log(res);
-  })
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <Button onClick={fetchNext} type="primary">登录</Button>
-          <Button onClick={fetchRegister} type="primary">注册</Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LoginApp} />
+          <Route path="/login" component={AppLayout} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
-
-export default class componentName extends Component {
+import React from 'react';
+import Style from './index.scss';
+import SignIn from './components/signIn'
+import SignUp from './components/signUp'
+import BackgLine from './components/BackgLine'
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSignUp: true
+    }
+  }
   render() {
     return (
-      <div>
-        
-      </div>
+      <main className={Style.login}>
+        <BackgLine />
+        <article className="login_info">
+          <section className="descript">
+            <div className="photo" />
+          </section>
+          <section className="login_dialog">
+            {
+              this.state.isSignUp ?
+              <SignIn /> : <SignUp toggleSign={this.toggleSign.bind(this)} />
+            }
+          </section>
+        </article>
+      </main>
     )
   }
 }
+
+export default Login;
+
